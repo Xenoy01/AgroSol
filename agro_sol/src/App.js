8a12a99b05d9b74369fd78da4d5c7aa85041f781
Xenoy01 <adarshkhatei2017@gmail.com>
2024-07-25 10:32:48 +0530

    initial commit

---
+++ agro_sol/src/App.js
@@ -0,0 +1,10 @@
+// import logo from './logo.svg';
+// import './App.css';
+import { AgroNavbar } from './components/Navbar/agronavbar';
+function App() {
+  return (
+    <AgroNavbar/>
+  )
+}
+
+export default App;
---
+++ agro_sol/src/components/Navbar/agronavbar.js
@@ -0,0 +1,8 @@
+
+export function AgroNavbar(){
+    return(
+        <nav class="navbar navbar-light bg-light">
+            <img src="components\Navbar\agrosol-high-resolution-logo.png" alt=""/>
+        </nav>
+    )
+}
